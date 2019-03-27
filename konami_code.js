bcbe4786e52dfd13170ad102a546e37bda343250
Connie Chan <conniechan.10@gmail.com>
2019-03-27 22:45:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,36 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  
+  // 1. We want to show a message if they get the key combo exactly right
+  function onKeyDown(event) {
+    // did they get the right key code?
+    if (event.keyCode === code[tracker]) {
+      tracker++ 
+    } else {
+      // if they get it wrong
+      tracker = 0
+    }
+    if (tracker === 10) {
+      alert ('Nice Job')
+  }
+  }
+                            // event,   callback
+  document.addEventListener('keydown', onKeyDown)
+  
+  // 2. If they mess up anywhere in the middle, they need to start over
+  
+  // create a sequence that will reset if they get it wrong
+  // if (event.keyCode === code[tracker]) {
+    // tracker = tracker + 1
+    // if tracker === 9 -> then message
+  //} else {
+    // tracker = 0
+  //}
+  
 }
+
+
+init()
